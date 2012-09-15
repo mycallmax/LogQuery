2e80ae6874b0e9f854bc2fec7c0bb6a8fdacfc83
ksheeras <balaraj2@illinois.edu>
2012-09-15 16:59:36 -0500

    Update Log Generator

--- LogGenerator.java
+++ LogGenerator.java
@@ -7,13 +7,14 @@ public class LogGenerator
 {
 	//private data members
 	private final static Logger LOGGER = Logger.getLogger(LogGenerator.class.getName());
-	//Log File name
-	private String FileName; 
 	// File size in MB
 	private int    FileSize;
+	// Machine ID
+	private int MachineID;
+
+	private String FileName;
+	File file;
 	private FileHandler fileHandler;
-	// Pattern types include "uniform" , "frequent" and "rare"
-	private String PatternType;
 	// Total log messages logged already
 	private int TotalMessages = 0;
 	// indicates if the logging should continue or not
@@ -21,133 +22,68 @@ public class LogGenerator
 
 	// class functions
 	// Init class members 
-	LogGenerator(String FileName, int FileSize, String PatternType)
+	LogGenerator(int FileSize, int MachineID)
 	{
-		this.FileName = "logs/" + FileName + "_" + FileSize + "MB_" + PatternType;
+		this.MachineID = MachineID;
+		this.FileName = "machine." + MachineID + ".log_" + FileSize + "MB";
 		this.FileSize = FileSize;
-		this.PatternType = PatternType;
+		file = new File(FileName);
 	}
 
 	// Update the run Status
 	private void updateRunStatus()
 	{
-		File file = new File(FileName);
 		// Get the number of bytes in the file
 		long length = file.length();
 		if(length > (FileSize * 1024 * 1024) )
 			runStatus = false;
 	}
 
-	// generates the logfile with "frequent" pattern
-	// generates the INFO message 98% of times
-	private void generateLogFrequent()
+	private void generateLog()
 	{
-		int num1;  int num2;
+		int num1 = 0; int num2; int SomeMachineID1 = 0; int SomeMachineID2 = 0;
 		Random gen1 = new Random(System.currentTimeMillis());
 		Random gen2 = new Random(System.currentTimeMillis());
 		while(runStatus)
 		{
+			num2 = gen2.nextInt(4) + 1;
+			while(num2 == MachineID)
+			{
+				num2 = gen2.nextInt(4) + 1;
+			}
+			if(num2 > MachineID)
+			{
+				SomeMachineID1 = MachineID;
+				SomeMachineID2 = num2;
+			}
+			else
+			{
+				SomeMachineID1 = num2;
+				SomeMachineID2 = MachineID;
+			}			
 			num1 = gen1.nextInt(100);
-			if(num1 > 1)
+			if(num1 >= 30)
 			{
-				LOGGER.info("This is informational");
+				// Log frequent messages
+				if(num1%2 == 0)
+					LOGGER.info("This is informational.  MachineID:" + MachineID + " SomeMachineIDs: " + SomeMachineID1 + "," + SomeMachineID2);
+				else
+					LOGGER.warning("This is warning. MachineID: " + MachineID + "  SomeMachineIDs: " + SomeMachineID1 + "," + SomeMachineID2);
+			}
+			else if (num1 >= 5 && num1 < 30)
+			{
+				// Log somewhat frequent messages
+				if(num1%2 == 0)
+					LOGGER.fine("This is fine.  MachineID: " + MachineID + "  SomeMachineIDs: " + SomeMachineID1 + "," + SomeMachineID2);
+				else
+					LOGGER.finest("This is finest. MachineID: "+  MachineID + "  SomeMachineIDs: " + SomeMachineID1 + "," + SomeMachineID2);
 			}
 			else
 			{
-				num2 = gen2.nextInt(4);
-				switch(num2)
-				{
-					case 0: 
-						LOGGER.severe("Attention required.");
-						break;
-					case 1:
-						LOGGER.warning("Do this carefully.");
-						break;
-					case 2:
-						LOGGER.fine("you are good.");
-						break;
-					case 3:
-						LOGGER.finest("Very delicate.");
-						break;
-					default:
-						;
-				}
-				TotalMessages = TotalMessages + 1;
-				if(TotalMessages % 5 == 0)
-					updateRunStatus();
+				// Log rare message
+				LOGGER.severe("This is severe. MachineID: " + MachineID + "  SomeMachineIDs: " + SomeMachineID1 + "," + SomeMachineID2);
 			}
-		}
-	}
 
-	// generates the logfile with "rare" pattern
-	// generates the SEVERE message 0.1% of times
-	private void generateLogRare()
-	{
-		int num1; int num2;
-		Random gen1 = new Random(System.currentTimeMillis());
-		Random gen2 = new Random(System.currentTimeMillis());
-		while(runStatus)
-		{
-			num1 = gen1.nextInt(1000);
-			if(num1 == 0)
-			{
-				LOGGER.severe("Attention required.");
-			}
-			else
-			{
-				num2 = gen2.nextInt(4);
-				switch(num2)
-				{
-					case 0:
-						LOGGER.info("This is informational");
-						break;
-					case 1:
-						LOGGER.warning("Do this carefully.");
-						break;
-					case 2:
-						LOGGER.fine("you are good.");
-						break;
-					case 3:
-						LOGGER.finest("Very delicate.");
-						break;
-					default:
-						;
-				}
-				TotalMessages = TotalMessages + 1;
-				if(TotalMessages % 5 == 0)
-					updateRunStatus();
-			}
-		}
-	}
-
-	// generates a logfile with uniform distribution
-	private void generateLogUniform()
-	{
-		int num1;
-		Random gen1 = new Random(System.currentTimeMillis());
-		while(runStatus)
-		{
-			num1 = gen1.nextInt(5);
-			switch(num1)
-			{
-				case 0:
-					LOGGER.info("This is informational");
-					break;
-				case 1:
-					LOGGER.warning("Do this carefully.");
-					break;
-				case 2:
-					LOGGER.fine("you are good.");
-					break;
-				case 3:
-					LOGGER.finest("Very delicate.");
-					break;
-				case 4:
-					LOGGER.severe("Attention required.");
-					break;
-				default:
-					;
-			}
 			TotalMessages = TotalMessages + 1;
 			if(TotalMessages % 5 == 0)
 				updateRunStatus();
@@ -177,18 +113,13 @@ public class LogGenerator
 	private void generate()
 	{
 		SetFileHandler();
-		if(PatternType.equals("frequent"))
-			generateLogFrequent();
-		else if (PatternType.equals("rare"))
-			generateLogRare();
-		else
-			generateLogUniform();
+		generateLog();
 	}
 
 	// Main Funtion
 	public static void main(String[] args)
 	{
-		LogGenerator MyLogGenerator = new LogGenerator(args[0],Integer.parseInt(args[1]),args[2]);
+		LogGenerator MyLogGenerator = new LogGenerator(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
 		MyLogGenerator.generate();
 	}
 } 
